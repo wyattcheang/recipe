1ac92d1a44b9aba6cbad752a0e170b281b90468c
wyattcheang <wyattcheangwaihoe@icloud.com>
2024-10-10 14:52:43 +0800

    Add existing project files to Git

---
+++ recipe/recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeApp.swift
+//  recipe
+//
+//  Created by Wyatt Cheang on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ recipeTests/recipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  recipeTests.swift
+//  recipeTests
+//
+//  Created by Wyatt Cheang on 09/10/2024.
+//
+
+import Testing
+@testable import recipe
+
+struct recipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
